82be32bb2fe2208c4487e6c0a7677978f7d02aea
hallowseph <146803230+hallowseph@users.noreply.github.com>
2024-03-01 11:53:18 +1300

    Update Array.java
    completed the insertToArray() method, added a main method to test the code.

--- DSA/Lab1/src/lab1/Array.java
+++ DSA/Lab1/src/lab1/Array.java
@@ -1,8 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lab1;
+
+import java.util.Arrays;
 
 /**
  *
@@ -10,22 +8,28 @@ package lab1;
  */
 public class Array {
 
-    int index = 0;
-//to find right position for inserting a number and position will be stored in the index
-    for(_____________ ;
-    ____________________________________ ;
-    index
-    ++);
-// shift numbers to make space to insert a new number
-for(___________________ ;
-    ____________________ ;
-    _______________
+    public static void main(String[] args) {
+        int[] array = {1, 3, 5, 7, 9};
+        int size = 5;
+        int numberToInsert = 4;
 
-    )
-{
-_____________________________________;
+        System.out.println("Original Array: " + Arrays.toString(array));
+
+        insertToArray(numberToInsert, array, size);
+
+        System.out.println("Array after insertion: " + Arrays.toString(array));
     }
-// insert a new number
-    list 
-    [index] = number ;
+
+    public static void insertToArray(int number, int[] list, int size) {
+        int index = 0;
+        //to find right position for inserting a number and position will be stored in the index
+        for (index = 0; index < size && list[index] < number; index++);
+        // shift numbers to make space to insert a new number
+        for (int i = size - 1; i > index; i--) 
+        {
+            list[i] = list[i - 1];
+        }
+        // insert a new number
+            list[index] = number;
+    }
 }
