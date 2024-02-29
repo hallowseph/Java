fef9ea43fe7435a1277b052f8c6f6cc62bac26d3
hallowseph <146803230+hallowseph@users.noreply.github.com>
2024-02-29 14:04:19 +1300

    Update Pattern.java
    now prints a diamond pattern but changed the diamond(6,6) to diamond(6,1) in the main function

--- DSA/Lab1/src/lab1/Pattern.java
+++ DSA/Lab1/src/lab1/Pattern.java
@@ -14,33 +14,39 @@ public class Pattern {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        diamond(6,6);
+        diamond(6, 1);
     }
-    
-    public static void repeatlyPrint(int amount,String pattern){
-        if(amount>0)
-        {
+
+    public static void repeatlyPrint(int amount, String pattern) 
+    {
+        if (amount > 0) {
             //print a pattern once
-            System.out.println(pattern);
+            System.out.print(pattern);
             //recursively call the function itself
-            repeatlyPrint(amount-1,pattern);
+            repeatlyPrint(amount - 1, pattern);
         }
     }
-    
-    public static void diamond(int totalHeight, int currentRow){
+
+    public static void diamond(int totalHeight, int currentRow) {
         //print spaces(outside of diamond)
         repeatlyPrint(totalHeight - currentRow, " ");
-        //print asterisks and spaces
-        repeatlyPrint(2 * currentRow - 1, "*");
+        
+        //print asterisks and spaces       
+        repeatlyPrint(currentRow, "* ");
+        
         System.out.println();
-        if(currentRow < totalHeight)
+
+        if (currentRow < totalHeight) 
         {
             diamond(totalHeight, currentRow + 1);
         }
+        
         //print spaces
-        repeatlyPrint(totalHeight - currentRow, " ");
+        repeatlyPrint(totalHeight-currentRow, " ");
+        
         //print asterisks and spaces
-        repeatlyPrint(2 * currentRow - 1, "*");
+        repeatlyPrint(currentRow, "* ");
+        
         System.out.println();
     }
 }
