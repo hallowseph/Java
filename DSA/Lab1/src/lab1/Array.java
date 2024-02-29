df44d086c90976f0581ff6dfda6e7a1ddc595b26
hallowseph <146803230+hallowseph@users.noreply.github.com>
2024-02-29 15:05:21 +1300

    Update Array.java
    copied the lab contents onto the java file

--- DSA/Lab1/src/lab1/Array.java
+++ DSA/Lab1/src/lab1/Array.java
@@ -6,8 +6,26 @@ package lab1;
 
 /**
  *
- * @author josep
+ * @author xxg8089
  */
 public class Array {
-    
+
+    int index = 0;
+//to find right position for inserting a number and position will be stored in the index
+    for(_____________ ;
+    ____________________________________ ;
+    index
+    ++);
+// shift numbers to make space to insert a new number
+for(___________________ ;
+    ____________________ ;
+    _______________
+
+    )
+{
+_____________________________________;
+    }
+// insert a new number
+    list 
+    [index] = number ;
 }
