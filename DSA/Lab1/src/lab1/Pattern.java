5401921fcfa72e9a5b81c9ca06eaf1530c2c540f
hallowseph <146803230+hallowseph@users.noreply.github.com>
2024-03-01 11:24:08 +1300

    Update Pattern.java
    cleaned up the code

--- DSA/Lab1/src/lab1/Pattern.java
+++ DSA/Lab1/src/lab1/Pattern.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package lab1;
 
 /**
@@ -9,10 +5,6 @@ package lab1;
  * @author xxg8089
  */
 public class Pattern {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         diamond(6, 6);
     }
