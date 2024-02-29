1277c97766f1c254afe57a761500961e80309474
hallowseph <146803230+hallowseph@users.noreply.github.com>
2024-03-01 12:23:36 +1300

    Update Binary.java
    main method initializes a decimal number of your preference then calls the  decimalToBinary method to convert the decimal number to binary form. decimalToBinary method - if the decimal number is 0 it returns an empty string. else if it is not 0 it right-shifts the number, bitwise, then concatenate

--- DSA/Lab1/src/lab1/Binary.java
+++ DSA/Lab1/src/lab1/Binary.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lab1;
 
 /**
  *
- * @author josep
+ * @author xxg8089
  */
 public class Binary {
-    
+    public static void main(String[] args) {
+        int decimalNumber = 8; //replace with the desired decimal number
+        String binaryForm = decimalToBinary(decimalNumber);
+        System.out.println("Binary form of: " + decimalNumber
+                            + " is: " + binaryForm);
+    }
+    public static String decimalToBinary(int number){
+        if(number == 0){
+            return "";
+        }else{
+            return decimalToBinary(number >> 1) + (number & 1);
+        }
+    }
 }
