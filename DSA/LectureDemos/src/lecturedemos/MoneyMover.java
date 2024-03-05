ce25feeb85ce0e5da7a67837b8c641b74449ccf6
hallowseph <146803230+hallowseph@users.noreply.github.com>
2024-03-05 18:48:13 +1300

    Created MoneyMoverDemo
    houses the main method for the money mover class

---
+++ DSA/LectureDemos/src/lecturedemos/MoneyMover.java
@@ -0,0 +1,16 @@
+package lecturedemos;
+
+/**
+ *
+ * @author josep
+ */
+public class MoneyMover {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
