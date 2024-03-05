b9e5f0515e4a7707562a0e8925bc3f9e184c05c7
hallowseph <146803230+hallowseph@users.noreply.github.com>
2024-03-06 10:33:28 +1300

    Created Q1 of Lab2

---
+++ DSA/Lab1/src/lab2/Extend_thread.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab2;
+
+/**
+ *
+ * @author xxg8089
+ */
+public class Extend_thread extends Thread {
+
+    int id;
+    int[] shared_array;
+    boolean done = false;
+    int booked = 0;
+
+    public Extend_thread(int id, int[] shared_array) {
+        this.id = id;
+        this.shared_array = shared_array;
+    }
+
+    public void run() {
+        for (int i = 0; i < shared_array.length; i++) {
+            book();
+        }
+    }
+
+    public synchronized void book() {
+        int i = 0;
+        //looking for an empty seat
+        for (; i < shared_array.length && shared_array[i] != 0; i++);
+        // check the seat available before booking
+        if (i < shared_array.length) {
+            shared_array[i] = id; //booking
+            booked++; //increase number of seats booked
+        }
+    }
+}
+
+
---
+++ DSA/Lab1/src/lab2/Extend_threadmain.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab2;
+
+/**
+ *
+ * @author josep
+ */
+public class Extend_threadmain {
+    public static void main(String[] args) {
+                
+        int[] seats = new int[1000];
+        
+        for(int i = 0; i < seats.length; i++)
+        {
+            seats[i] = 0; //empty the booking of a seat
+        }
+        
+        Extend_thread et1 = new Extend_thread(1, seats);
+        Extend_thread et2 = new Extend_thread(2, seats);
+        Extend_thread et3 = new Extend_thread(3, seats);
+        
+        et1.start();
+        et2.start();
+        et3.start();
+        
+        //waiting for all threads to finish
+        while(et1.isAlive() || et2.isAlive()||et3.isAlive()); 
+                
+        for(int i = 0; i < seats.length; i++)
+        {
+            System.out.print(seats[i] +" ");
+            
+            if(i%10 == 9)
+                System.out.println();
+        }
+        System.out.println("ET 1 booked: "+et1.booked+" ET 2 booked: "+et2.booked +" ET 3 booked: "+et3.booked+"\nTotal booked: " +(et1.booked+et2.booked+et3.booked));
+        
+}
+}
