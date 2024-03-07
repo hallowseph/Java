2113a54eacc06e5fdf20f2376c53ffc952649307
hallowseph <146803230+hallowseph@users.noreply.github.com>
2024-03-07 16:26:57 +1300

    Update BugFix.java
    correct working code, set 2 examples for finding the target value in the array. returns -1 if not found else it returns the index of the target value

--- DSA/Lab1/src/lab2/BugFix.java
+++ DSA/Lab1/src/lab2/BugFix.java
@@ -1,30 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lab2;
 
 import static java.util.Collections.list;
 
 /**
  *
- * @author josep
+ * @author xxg8089
  */
 public class BugFix {
-    
+
     public static void main(String[] args) {
-        
+        //target value is found in the array
+        int[] array1 = {1, 2, 3, 4, 5};
+        int target1 = 3;
+        int result1 = findInArray(array1, target1);
+        System.out.println("Index of " + target1 + ": " + result1);
+
+        //target value is not found in the array
+        int[] array2 = {10, 20, 30, 40 ,50};
+        int target2 = 15;
+        int result2 = findInArray(array2, target2);
+        System.out.println("Index of " + target2 + ": " + result2);
     }
     //method findInArray() finds a targetValue in the array which is named “list” and returns index of //targetValue in the array. It returns -1 if it cannot find targetValue.
-   
-private static int findInArray(int[] list, int targetValue) 
-    {
+
+    private static int findInArray(int[] list, int targetValue) {
         int index = 0;
-        
-        for(; list[index]!= targetValue && index < list.length; index++);
-        
-        if(index <list.length)
+
+        for (; index < list.length && list[index] != targetValue; index++);
+
+        if (index < list.length) {
             return index;
+        }
         return -1; //not found
     }
 
