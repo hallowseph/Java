e7132a0846702db09876b6e71dde79c60905c2de
hallowseph <146803230+hallowseph@users.noreply.github.com>
2024-03-13 09:44:16 +1300

    Created additional classes for Lab3

---
+++ DSA/Labs/src/lab3/DSA_Quiz3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab3;
+
+/**
+ *
+ * @author josep
+ */
+public class DSA_Quiz3 {
+    public static void main(String[] args) {
+        String[] stringList = {"Hello", "Hello", "Data", "Structures", "and", "Algorithms", "Students"};
+        LinkedSet<String> linkedSet = new LinkedSet();
+        for(int i = 0; i < stringList.length; i++){
+            linkedSet.add(stringList(i));
+        }
+        
+        System.out.println(linkedSet.toString());
+        System.out.println(linkedSet.toStringReversely());
+    }
+}
