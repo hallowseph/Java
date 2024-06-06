a8a20512f1b9dc63e13a0f9caa4367949137bc11
Joseph Reanzares <xxg8089@autuni.ac.nz>
2024-06-06 13:44:26 +1200

    Created Circ. Queue and LinkedQueue.java

---
+++ DSA/Labs/Week1Lab/src/LABS/LinkedQueue.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LABS;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author josep
+ */
+public class LinkedQueue<T> {
+    private LinkedList<T> queue;
+    
+    public LinkedQueue(){
+        queue = new LinkedList<>();
+    }
+    
+    public void enqueue(T element){
+        queue.addLast(element);
+    }
+    
+    public T dequeue(){
+        if(isEmpty()){
+            return null;
+        }
+        return queue.removeFirst();
+    }
+    
+    public T first(){
+        if(isEmpty()){
+            return null;
+        }
+        return queue.getFirst();
+    }
+    
+    public boolean contains(T element){
+        return queue.contains(element);
+    }
+    
+    public boolean isEmpty(){
+        return queue.isEmpty();
+    }
+    
+    public int size(){
+        return queue.size();
+    }
+    
+}
